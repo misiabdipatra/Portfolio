26a4f400c4012aaa81f54a9901e69f9f80c1bbcc
Misia Callista Abdipatra <88731878+misiabdipatra@users.noreply.github.com>
2025-02-14 09:35:02 +0700

    Add files via upload

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -11,15 +11,15 @@ export const Contact = () => {
             </div>
             <ul className={styles.links}>
                 <li className={styles.link}>
-                    <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-                    <a href="misiabdipatra@gmail.com">misiabdipatra@gmail.com</a>
+                    <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon"/>
+                    <a href="https://mail.google.com/mail/?view=cm&fs=1&to=misiabdipatra@gmail.com" target="_blank">misiabdipatra@gmail.com</a>
                 </li>
                 <li className={styles.link}>
-                    <img src={getImageUrl("contact/linkedinIcon.png")} alt="Linkedin icon" />
+                    <img src={getImageUrl("contact/linkedinIcon.png")} alt="Linkedin icon"/>
                     <a href="https://www.linkedin.com/in/misiaabdipatra">Misia Callista Abdipatra</a>
                 </li>
                 <li className={styles.link}>
-                    <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+                    <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon"/>
                     <a href="https://github.com/misiabdipatra">misiabdipatra</a>
                 </li>
             </ul>
